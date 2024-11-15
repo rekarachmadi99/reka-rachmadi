dc501c788fc3da8f0811bcfd50299d62a44d9818
Reka Rachmadi Apriansyah <reka.rachmadi@gmail.com>
2024-11-15 14:23:57 +0700

    Update Portfolio

---
+++ src/components/PortfolioList.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+
+// Define types for the data structure
+interface Item {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+interface PortfolioListProps {
+  data: Item[];
+}
+
+const PortfolioList: React.FC<PortfolioListProps> = ({ data }) => {
+  return (
+    <div>
+      {data.map((item, index) => (
+        <ItemCard key={index} item={item} />
+      ))}
+    </div>
+  );
+};
+
+const ItemCard: React.FC<{ item: Item }> = ({ item }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const handleViewClick = () => {
+    window.open(item.link, "_blank");
+  };
+
+  const shortDescription =
+    item.description.length > 250
+      ? `${item.description.substring(0, 250)}...`
+      : item.description;
+
+  return (
+    <div className="flex flex-col md:flex-row gap-2 m-4 p-4 shadow dark:shadow-gray-200 rounded">
+      <div className="md:w-4/6 w-full">
+        <img
+          src={item.image}
+          className="w-full object-cover"
+          alt="Sistem Informasi Apotik"
+        />
+      </div>
+
+      <div className="md:w-2/6 w-full flex flex-col h-full">
+        <h1 className="text-black dark:text-white text-2xl md:text-4xl font-serif mb-4">
+          {item.title}
+        </h1>
+        <p
+          className="mb-4 text-gray-600 dark:text-gray-300 font-serif"
+          dangerouslySetInnerHTML={{
+            __html: isExpanded ? item.description : shortDescription,
+          }}
+        ></p>
+
+        {/* Toggle Button for Read More */}
+        {item.description.length > 250 && (
+          <button
+            onClick={toggleReadMore}
+            className="text-blue-600 dark:text-blue-400 font-serif underline"
+          >
+            {isExpanded ? "Show Less" : "Read More"}
+          </button>
+        )}
+
+        <div className="mt-auto">
+          <button
+            onClick={handleViewClick}
+            className="flex items-center px-4 py-2 space-x-2 bg-white text-black border-2 border-black font-serif w-full md:w-auto transition-colors duration-300 ease-in-out 
+              dark:bg-black dark:border-white dark:text-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black"
+          >
+            <span>View</span>
+            <FaArrowRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioList;
--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -1,18 +1,39 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import { FaArrowRight } from "react-icons/fa";
+import PortfolioList from "../../components/PortfolioList";
 
 const Portfolio: React.FC = () => {
   const data = [
     {
-      title: "Sistem Informasi Manajemen Apotik Umbulharjo",
-      description: "Deskripsi singkat mengenai sistem informasi apotek ini.",
+      title: "Accreditation Quality Information System application",
+      description: `
+        The Quality Information System of the Health Office of Tasikmalaya Regency features several developed functions:
+        <ul>
+          <li><strong>Clinic management</strong></li>
+          <li><strong>Service quality monitoring</strong></li>
+          <li><strong>Employee management</strong></li>
+        </ul>
+        <br><strong>Tech stack:</strong> Web Development · PHP · Laravel · JavaScript · Bootstrap · MySQL
+      `,
       image: "https://dummyimage.com/600x400/000/fff",
       link: "",
     },
     {
-      title: "Sistem Informasi Manajemen Apotik Tasikmalaya",
-      description: "Deskripsi singkat mengenai sistem informasi apotek ini.",
+      title:
+        "Information System for Processing Student Report Values In Public Elementary School 2 Kersanagara",
+      description: `
+        The student report assessment information system is developed by implementing multi-role access for teachers, school principals, subject teachers, administrative staff, and parents. It features several built-in functionalities:
+        <ul>
+          <li><strong>Student data management</strong></li>
+          <li><strong>Teacher data management</strong></li>
+          <li><strong>Extracurricular activity data management</strong></li>
+          <li><strong>Student assessment</strong></li>
+          <li><strong>Extracurricular assessment</strong></li>
+          <li><strong>Student attendance</strong></li>
+        </ul>
+        <br><strong>Tech stack:</strong> Web Development · CodeIgniter · PHP · JavaScript · Bootstrap · MySQL
+      `,
       image: "https://dummyimage.com/600x400/000/fff",
       link: "",
     },
@@ -24,6 +45,32 @@ const Portfolio: React.FC = () => {
                     <br><br><strong>Tech stack:</strong> Web Development · CodeIgniter · PHP · JavaScript · Bootstrap · MySQL`,
       image: "https://dummyimage.com/600x400/000/fff",
       link: "",
+    },
+    {
+      title: "Financial System And Website Profile",
+      description: `
+        The system built for Lazismu Umbulharjo Yogyakarta includes a financial system and a profile website, encompassing the following features:
+        <br><br><strong>Financial System:</strong>
+        <ul>
+          <li><strong>Dashboard feature</strong> to display information on fund expenditures, income, petty cash, and main cash.</li>
+          <li><strong>Data management feature</strong> for income, expenditures, petty cash, main cash, donors, and employees, allowing input, update, deletion, and data verification.</li>
+          <li><strong>Account management feature</strong> including adding accounts and displaying account data.</li>
+          <li><strong>Record input feature</strong> for transaction system management history conducted by administrators or employees.</li>
+          <li><strong>System Access/Administration feature</strong> to manage accounts and access rights within the system.</li>
+          <li><strong>Preview and export reports to Excel</strong>.</li>
+        </ul>
+        <br><strong>Website Profile:</strong>
+        <ul>
+          <li><strong>Data input feature</strong> for Lazismu Umbulharjo's profile, including vision-mission statements, documentation of activities, partners, news, and accounts.</li>
+          <li><strong>Program and slider feature</strong> for highlighting programs and events.</li>
+          <li><strong>Contact feature</strong> linked to WhatsApp for easy communication.</li>
+          <li><strong>Suggestion box feature</strong> for providing feedback to Lazismu Umbulharjo.</li>
+          <li><strong>File upload and download feature</strong> for archival documents (soft files).</li>
+        </ul>
+        <br><strong>Tech stack:</strong> Web Development · CodeIgniter · PHP · JavaScript · Bootstrap · Git · MySQL
+      `,
+      image: "https://dummyimage.com/600x400/000/fff",
+      link: "https://www.canva.com/design/DAGJWrCuiqE/XgL4Vit7kYekZabpwC5_6g/edit",
     },
   ];
   return (
@@ -38,10 +85,8 @@ const Portfolio: React.FC = () => {
             <div className="border border-black dark:border-white w-1/3 my-5"></div>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-1 gap-4 pb-8 px-4">
-            <div>
-              <input type="text" className="p-2" />
-            </div>
-            {data.map((items) => {
+            <PortfolioList data={data} />
+            {/* {data.map((items) => {
               return (
                 <div className="flex flex-col md:flex-row gap-4 p-4 shadow dark:shadow-gray-200 rounded">
                   <div className="md:w-4/6 w-full">
@@ -73,7 +118,7 @@ const Portfolio: React.FC = () => {
                   </div>
                 </div>
               );
-            })}
+            })} */}
           </div>
         </div>
       </div>
